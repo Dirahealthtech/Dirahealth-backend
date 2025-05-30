682a45017b26788fd423ea13f767f4f1f3a0f47a
magnus.coder <kelimbth@gmail.com>
2025-05-30 21:49:39 +0300

    feat: created user schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,8 @@
+from .auth_schema import (
+    ConfirmResetPasswordSchema,
+    LoginRequest,
+    LoginResponse,
+    RequestEmailVerificationSchema,
+    ResetPasswordSchema,
+)
+from .user_schema import BaseUser, CreateUser, CreatedUserResponse, UserResponse
---
+++ app/schemas/user_schema.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr, Field, StringConstraints
+from typing import Annotated, Optional
+
+
+validated_mobile_num = Annotated[str, StringConstraints(min_length=10, max_length=15, pattern=r'^\+?[1-9]\d{1,14}$')]
+
+class BaseUser(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: validated_mobile_num
+    hashed_password: str = Field(min_length=8)
+
+
+    class Config:
+        from_attributes = True
+
+
+class CreateUser(BaseUser):
+    """ This is a schema to create a user account. """
+    pass
+
+
+class CreatedUserResponse(BaseUser):
+    """ Response data for a newly created user. """
+
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    role: str
+    is_verified: bool
+    hashed_password: str | None = Field(default=None, exclude=True)
+
+    class Config:
+        from_attributes = True
+
+
+class UserResponse(BaseUser):
+    id: int
+    is_verified: bool
+
