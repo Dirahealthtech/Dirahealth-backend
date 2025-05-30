ae18ba30fd67173aec800319132da1df1de9f24b
magnus.coder <kelimbth@gmail.com>
2025-05-30 15:45:27 +0300

    feat: add enums for UserRole, CustomerType, and AddressType

---
+++ app/enums.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class UserRole(str, enum.Enum):
+    CUSTOMER = "ustomer"
+    ADMIN = "admin"
+    SERVICE_TECH = "service_tech"
+    SUPPLIER = "supplier"
+
+
+class CustomerType(str, enum.Enum):
+    INDIVIDUAL = "individual"
+    HOSPITAL = "hospital"
+    HEALTHCARE_PROVIDER = "healthcare_provider"
+
+
+class AddressType(str, enum.Enum):
+    BILLING = "billing"
+    SHIPPING = "shipping"
+    BOTH = "both"
