add2d4fa0f8a7a5ad0349fd68fdc2efa39a0cd19
magnus.coder <kelimbth@gmail.com>
2025-05-30 14:07:10 +0300

    fix: import missing objects - Column and DateTime, and added a whitespace

--- app/models/base.py
+++ app/models/base.py
@@ -1,8 +1,10 @@
+from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
 
 Base = declarative_base()
+
 
 class TimeStampMixin:
     """
