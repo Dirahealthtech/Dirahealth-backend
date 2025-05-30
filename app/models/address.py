c6e01aa41914f8ea1430ca6ecaf9f4ea40eec3f6
magnus.coder <kelimbth@gmail.com>
2025-05-30 15:45:17 +0300

    fix: removed enum class and updated import path

--- app/models/address.py
+++ app/models/address.py
@@ -2,13 +2,8 @@ from sqlalchemy import Boolean, Column, Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 import enum
 
-from app.models.base import Base, TimeStampMixin
-
-
-class AddressType(str, enum.Enum):
-    BILLING = "billing"
-    SHIPPING = "shipping"
-    BOTH = "both"
+from ..models.base import Base, TimeStampMixin
+from ..enums import AddressType
 
 
 class Address(Base, TimeStampMixin):
