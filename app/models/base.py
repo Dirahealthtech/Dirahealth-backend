db9b11d08e8642a9dd51e04661ca3a5f66efb53e
magnus.coder <kelimbth@gmail.com>
2025-05-30 14:07:41 +0300

    style: format code by removing unnecessary whitespace

--- app/models/base.py
+++ app/models/base.py
@@ -15,9 +15,9 @@ class TimeStampMixin:
         - `updated_at`: Automatically set to the current time when the record is created,
            and automatically updated when the record is updated.
     """
- 
+
     # Automatically set on insert using the database's current timestamp
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    
+
     # Automatically set on insert AND updated on any update
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
