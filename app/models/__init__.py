458ea43f87bbc4041a7a2e5d4f52d02e9724178e
nick222rt <nicky.rop@techloom.co.ke>
2025-06-03 14:28:02 +0300

    register new models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,17 @@ from .category import Category
 from .customer_profile import CustomerProfile
 from .prescription import Prescription
 from .user import User
+from .appointment import Appointment
+from .inventory import InventoryTransaction
+from .order_item import OrderItem
+from .order_service import OrderService
+from .order import Order
+from .prescription import Prescription
+from .product import Product
+from .purchase_order_item import PurchaseOrderItem
+from .purchase_order import PurchaseOrder
+from .service import Service
+from .supplier import Supplier
 
 
 __all__ = [
@@ -11,4 +22,16 @@ __all__ = [
     "CustomerProfile",
     "Prescription",
     "User",
+    "Appointment",
+    "Inventory",
+    "OrderItem",
+    "OrderService",
+    "Order",
+    "Prescription",
+    "Product",
+    "PurchaseOrderItem",
+    "PurchaseOrder",
+    "Service",
+    "Supplier",
+    "InventoryTransaction"
 ]
