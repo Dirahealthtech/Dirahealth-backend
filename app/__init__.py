61b79c73376c60f09597c7a5ee67b2653e520a61
magnus.coder <kelimbth@gmail.com>
2025-06-04 15:18:33 +0300

    feat: registered routers for suppliers

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,7 @@ from app.exceptions import (
 from app.middleware.auth_middleware import CustomAuthMiddleWare
 from app.routers.auth import router as auth_router
 from app.routers.admin import router as admin_router
+from app.routers.suppliers import router as suppliers_router
 
 
 api_version = "v1"
@@ -52,7 +53,7 @@ app.add_middleware(
 # register endpoints
 app.include_router(auth_router, prefix=f'/api/{api_version}/auth', tags=["Authentication"])
 app.include_router(admin_router, prefix=f'/api/{api_version}/admin', tags=["Admin"])
-
+app.include_router(suppliers_router, prefix=f'/api/{api_version}/suppliers', tags=["Suppliers"])
 
 
 # register custom exceptions
