360d9443296886fb4017bc8cce7395eeb02d4d9e
magnus.coder <kelimbth@gmail.com>
2025-07-08 12:03:51 +0300

    feat: updated router to support both authenticated & anonymous users

--- app/routers/user_activity.py
+++ app/routers/user_activity.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
+from typing import List, Optional
 
-from ..core.dependencies import get_current_user, get_db
+from ..core.dependencies import get_anonymous_user, get_db
 from ..models import User
 from ..schemas.product import ProductResponse
 from ..services.user_activity_service import UserActivityService
 
-router = APIRouter()
+
+router = APIRouter(prefix='/activity')
 
 
 async def get_user_activity_service(db: AsyncSession = Depends(get_db)) -> UserActivityService:
@@ -26,7 +27,8 @@ async def get_user_activity_service(db: AsyncSession = Depends(get_db)) -> UserA
 
 @router.get("/top-picks", response_model=List[ProductResponse])
 async def get_top_picks(
-    current_user: User = Depends(get_current_user),
+    current_user: Optional[User] = Depends(get_anonymous_user),
+    anonymous_user: Optional[str] = Query(None),
     service: UserActivityService = Depends(get_user_activity_service),
 ):
     """
@@ -44,4 +46,20 @@ async def get_top_picks(
     Raises:
         HTTPException: If the user is not authenticated or an error occurs during retrieval.
     """
-    return await service.get_top_picks(current_user)
+    return await service.get_top_picks(current_user, anonymous_user)
+
+
+@router.get('/homepage', response_model=List[ProductResponse])
+async def get_products(service: UserActivityService = Depends(get_user_activity_service)):
+    """
+    Retrieve available products to users. This endpoint returns a list of products sold by suppliers
+    via the website.
+    """
+    return await service.get_products()
+
+@router.get('/product/{slug}', response_model=ProductResponse)
+async def get_product_details_by_slug(
+    slug: str,
+    service: UserActivityService = Depends(get_user_activity_service),
+):
+    return await service.get_product_by_slug(slug)
