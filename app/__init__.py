8ce4692cb5dadc563f028b8675df4deb88ce6535
nick222rt <nicky.rop@techloom.co.ke>
2025-08-06 10:50:23 +0300

    feat: user management implementation

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,7 @@ from app.routers.orders import router as orders_router
 from app.routers.user_activity import router as user_activity_router
 from app.routers.reviews import router as reviews_router
 from app.routers.homepage_sections import router as homepage_sections_router
+from app.routers.user_management import router as user_management_router
 
 
 api_version = "v1"
@@ -66,6 +67,7 @@ app.include_router(orders_router, prefix=f'/api/{api_version}/orders', tags=['Or
 app.include_router(user_activity_router, prefix=f'/api/{api_version}/user', tags=["User Activity"])
 app.include_router(reviews_router, prefix=f'/api/{api_version}/reviews', tags=["Reviews"])
 app.include_router(homepage_sections_router, prefix=f'/api/{api_version}', tags=["Homepage Sections"])
+app.include_router(user_management_router, prefix=f'/api/{api_version}', tags=["User Management"])
 
 
 # register custom exceptions
